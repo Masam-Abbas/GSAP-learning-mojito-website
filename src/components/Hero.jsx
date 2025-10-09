15cf1d8fac1c34a06d07ee47620a270c7c7bd08a
Masam Abbas <masamabbas040@gmail.com>
2025-10-09 23:16:54 +0500

    Refactor Hero component: add video refrence and update video source; enhance GSAP timeline for better scroll animation.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  const video_ref = React.useRef(video_ref);
+  const videoRef = useRef();
 
   const isMobile = useMediaQuery({ maxWidth: 768 });
 
@@ -47,8 +47,23 @@ const Hero = () => {
       .to(".left-leaf", { y: -200 }, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
-    const endValue = isMobile ? "bottom top" : "bottom 20%";
+    const endValue = isMobile ? "120% top" : "bottom top";
 
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
@@ -91,11 +106,11 @@ const Hero = () => {
 
       <div className="video absolute inset-0">
         <video
-          src="/video/input.mp4"
+          src="/videos/output.mp4"
           muted
           playsInline
           preload="auto"
-          ref={video_ref}
+          ref={videoRef}
         />
       </div>
     </>
