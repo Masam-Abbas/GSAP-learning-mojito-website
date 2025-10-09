ce11c820cd6eee85a173c5f08bc6bbc0e42e7191
Masam Abbas <masamabbas040@gmail.com>
2025-10-09 21:54:32 +0500

    Minor changes in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,13 @@ import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const video_ref = React.useRef(video_ref);
+
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paraSplit = new SplitText(".subtitle", { type: "lines" });
@@ -28,7 +33,8 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
+    gsap
+      .timeline({
         scrollTrigger: {
           trigger: "#hero",
           start: "top top",
@@ -39,6 +45,10 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "bottom top" : "bottom 20%";
+
   }, []);
   return (
     <>
@@ -78,6 +88,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          src="/video/input.mp4"
+          muted
+          playsInline
+          preload="auto"
+          ref={video_ref}
+        />
+      </div>
     </>
   );
 };
